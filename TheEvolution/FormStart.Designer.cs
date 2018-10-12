53041d8f6d3033ec3fdc3bc31712178421d43e12
Sean Wang <ggg44473@gmail.com>
2018-10-12 09:55:11 +0800

    Rename Form1 to FormStart

--- TheEvolution/FormStart.Designer.cs
+++ TheEvolution/FormStart.Designer.cs
@@ -1,5 +1,5 @@
 ﻿namespace TheEvolution {
-    partial class Form1 {
+    partial class FormStart {
         /// <summary>
         /// Required designer variable.
         /// </summary>
@@ -23,10 +23,17 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // FormStart
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "FormStart";
+            this.Text = "The Evolution";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- TheEvolution/FormStart.cs
+++ TheEvolution/FormStart.cs
@@ -9,8 +9,8 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace TheEvolution {
-    public partial class Form1 : Form {
-        public Form1() {
+    public partial class FormStart : Form {
+        public FormStart() {
             InitializeComponent();
         }
     }
