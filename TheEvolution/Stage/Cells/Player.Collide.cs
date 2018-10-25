17dfe883c00c483212473189ae330f621aaad50c
Sean Wang <ggg44473@gmail.com>
2018-10-25 19:53:44 +0800

    Add ImageContainer
    Resolved #18

--- TheEvolution/Stage/Cells/Player.Collide.cs
+++ TheEvolution/Stage/Cells/Player.Collide.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 using TheEvolution.Core;
 
 namespace TheEvolution.Stage.Cells {
     partial class Player : ICollideFood {
 
         public void CollideFood() {
-
+            MessageBox.Show("Player collided Food");
         }
     }
 }
